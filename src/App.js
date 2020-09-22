d273b9cdd407ad8bd43d142268a0a28cb63142a6
Akarsh Barar <barar.akarsh8@gmail.com>
2020-09-23 00:32:36 +0530

    HashRouter

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { HashRouter as Router, Switch, Route } from "react-router-dom"
 
 
 
